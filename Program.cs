3dc8ce15f0488e5640905d1002e1ec1820427872
Thiago <thiagolopespereiralp1@gmaail.com>
2023-04-20 20:37:59 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("Digite uma frase:");
+string frase = Console.ReadLine();
+
+Console.WriteLine("Digite um número:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+string fraseMaiuscula = frase.ToUpper(); 
+
+string fraseAlterada = AumentarLetrasNoAlfabeto(fraseMaiuscula, numero);
+
+Console.WriteLine("Frase original: " + frase);
+Console.WriteLine("Frase alterada: " + fraseAlterada);
+
+
+static string AumentarLetrasNoAlfabeto(string frase, int numero)
+{
+char[] letras = frase.ToCharArray();
+string novaFrase = "";
+
+foreach (char letra in letras)
+{
+if (char.IsLetter(letra)) 
+{
+char novaLetra = (char)(((letra - 'A' + numero) % 26) + 'A'); 
+
+novaFrase += novaLetra;
+}
+else
+{
+novaFrase += letra; 
+}
+}
+
+return novaFrase;
+}
+
